fd9b308637f38e2b15fa21ed7364be5c449ffbc8
kostm <anduser@k-merkulov.local>
2023-02-07 12:17:34 +0200

    first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button :class="$style.button" id="btn-remote" v-on:click="count+=2">
+      <slot>remote-simple's Button: {{ count }}</slot>
+    </button>
+    <counter />
+  </div>
+</template>
+
+<script lang="ts">
+import {Vue, Component} from "vue-property-decorator";
+import Counter from './Counter.vue';
+
+@Component({
+  components: {
+    Counter
+  }
+})
+export default class Button extends Vue {
+  public count: number = 0;
+}
+</script>
+<style module>
+.button {
+  background-color: #4caf50; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="counter">
+    <button id="btn-remote" v-on:click="count+=3">
+      <slot>remote-simple's Button: {{ count }}</slot>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import {Vue, Component} from "vue-property-decorator";
+
+@Component
+export default class Counter extends Vue {
+  public count: number = 0;
+}
+</script>
+
+<style scoped>
+
+</style>
